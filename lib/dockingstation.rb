46d80fe69d5d910642356f531b1b3814d11b364b
Nisar Tahir <nisar.tahir@gmail.com>
2013-11-19 21:11:26 +0000

    Added station bike storage limit

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,8 +2,9 @@ require 'Bike'
 
 class DockingStation
 
-  def initialize
+  def initialize(options = {})
     @bikes = []
+    @station_limit = options.fetch(:station_limit)
   end
 
   def bike_count
@@ -17,4 +18,11 @@ class DockingStation
   def release(bike)
     @bikes.delete bike
   end
+
+  def limit_reached?
+    if bike_count >= 15 && @station_limit >= 15
+      true
+    end 
+  end
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,7 +2,7 @@ require './lib/dockingstation.rb'
 
 describe DockingStation do
   
-  let(:station) { DockingStation.new }
+  let(:station) { DockingStation.new(:station_limit => 15) }
   let(:bike) { Bike.new }
 
   it "should have a docking station" do
@@ -14,5 +14,11 @@ describe DockingStation do
   it "should release a bike from the docking station" do
     station.release(bike)
     expect(station.bike_count).to eq(0)
+  end
+
+  it "should not allow more bikes to be stored than it's storage limit" do
+    expect(station).not_to be_limit_reached
+    15.times {station.dock(bike)}
+    expect(station).to be_limit_reached
   end
 end
