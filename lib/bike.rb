fe3bdc6177745f8ac9ea1a2eb892c4238bb0c796
Nisar Tahir <nisar.tahir@gmail.com>
2013-11-19 16:46:01 +0000

    Added fix method

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,14 +1,23 @@
 class Bike
   def initialize
     @break = false
+    @fixed = false
   end
 	
   def broken?
     @break
 	end
 
+  def fixed?
+    @fixed
+  end
+
   def break
     @break = true
   end
 
+  def fix
+    @break = false
+    @fixed = true
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,14 +1,21 @@
 require './lib/bike.rb'
 
 describe Bike do
+  
+  let(:bike) { Bike.new}
+
 	it "should not be broken after it has been created" do
-		bike = Bike.new
 	  expect(bike).not_to be_broken
 	end
 
   it "should be broken after we create it" do
-    bike = Bike.new
     bike.break
     expect(bike).to be_broken
+  end
+
+  it "should be fixed after the bike has been created" do
+    bike.break
+    bike.fix
+    expect(bike).not_to be_broken
   end
 end
