fb1217214ec23af225835c1eae7211b7606b042b
Nisar Tahir <nisar.tahir@gmail.com>
2013-11-19 16:11:29 +0000

    Added break method and initialize method for the Bike class

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,14 @@
 class Bike
-
-	def broken?
-
+  def initialize
+    @break = false
+  end
+	
+  def broken?
+    @break
 	end
+
+  def break
+    @break = true
+  end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,14 @@
 require './lib/bike.rb'
 
 describe Bike do
-it "should not be broken after it has been created" do
-	bike = Bike.new
-  expect(bike).not_to be_broken
-end
+	it "should not be broken after it has been created" do
+		bike = Bike.new
+	  expect(bike).not_to be_broken
+	end
+
+  it "should be broken after we create it" do
+    bike = Bike.new
+    bike.break
+    expect(bike).to be_broken
+  end
 end
